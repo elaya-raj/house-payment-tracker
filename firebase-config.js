7c281f40aea6f653c0de0d54901e39d464fc1ec8
Elayaraj <165200481+elaya-raj@users.noreply.github.com>
2026-02-28 21:58:19 -0800

    Update firebase-config.js
    updated file

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Firebase config for real-time sharing (house-tracker-2, asia-southeast1)
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDYea41Lufbr82VG7g8I21U2yx1hptNt04",
   authDomain: "house-tracker-2.firebaseapp.com",
   databaseURL: "https://house-tracker-2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "house-tracker-2",
   storageBucket: "house-tracker-2.firebasestorage.app",
   messagingSenderId: "145773530767",
-  appId: "1:145773530767:web:472e3cb2132428c792aafa",
-  measurementId: "G-5SZY9ZBLS4"
+  appId: "1:145773530767:web:472e3cb2132428c792aafa"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
